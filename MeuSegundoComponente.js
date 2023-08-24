34ab37554169aa51903c653edd273b4df7da1fd7
Guilherme Caetano <gcml1144@gmail.com>
2023-08-23 21:53:11 -0300

    explicações da aula

---
+++ MeuSegundoComponente.js
@@ -0,0 +1,16 @@
+
+
+// export default function MeuSegundoComponente(){
+//     function getName(){
+//         return "João"}
+
+//     return(
+//         <View>
+//             <text>{1 + 1}</text>
+//             <text>numero 1</text>
+//             <text>numero 1</text>
+//             <text>numero 1</text>
+//             <text>numero 1</text>
+//         </View>
+//     )
+// }
